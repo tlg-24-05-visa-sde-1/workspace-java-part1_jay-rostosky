9481ac75a8370ab909fb6854af46746d2ab65b3f
Jay Rostosky <jay.rostosky@tlgcohort.com>
2024-05-29 14:58:35 -0700

    Add fileReturn() as default method in TaxPayer

--- Lab09.1/src/com/hr/personnel/Executive.java
+++ Lab09.1/src/com/hr/personnel/Executive.java
@@ -12,4 +12,10 @@ public class Executive extends SalariedEmployee {
     public void work() {
         System.out.println(getName() + " enjoying a nice round of golf");
     }
+
+    // OPT-IN to override fileReturn() to do it differently
+    @Override  // interface TaxPayer
+    public void fileReturn() {
+        System.out.println("Return filed electronically");
+    }
 }
