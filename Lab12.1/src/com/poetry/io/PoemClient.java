cf94413d24e4266d57f26c8b5b8a00e32a92340d
Jay Rostosky <jay.rostosky@tlgcohort.com>
2024-06-04 07:59:17 -0700

    Lab complete - including use of Files class

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -9,7 +9,12 @@
 package com.poetry.io;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class PoemClient {
 
@@ -17,7 +22,7 @@ public class PoemClient {
      * To run one method at a time, uncomment the call to the one you want to execute.
      */
     public static void main(String[] args) {
-        // readPoem();
+        readPoem();
         // writePoem();
     }
 
@@ -34,13 +39,23 @@ public class PoemClient {
      * The try-with-resources below allows you to initialize the stream and auto-close it.
      */
     private static void readPoem() {
-        // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
-
+        try {
+            String poem = Files.readString(Path.of("famous-poem.txt"));
+            System.out.println(poem);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+
+/*        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {  // when it goes null, you are at EOF
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 
     /**
@@ -55,6 +70,13 @@ public class PoemClient {
      * Use a try-with-resources to initialize the stream and auto-close it.
      */
     private static void writePoem() {
-        // TODO
+        try (PrintWriter writer = new PrintWriter(new FileWriter("haiku.txt"))) {
+            writer.println("Me, Morton's Steakhouse");
+            writer.println("Had porterhouse, on the house");
+            writer.println("And NO garlic bread!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
